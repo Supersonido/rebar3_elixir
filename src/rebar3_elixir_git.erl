c5ec2931a65a0d0bfd4bc9c4b2d293111d201d61
Andres Mareca <andres.mareca@gmail.com>
2018-12-06 18:25:20 +0100

    Clean code

--- src/rebar3_elixir_git.erl
+++ src/rebar3_elixir_git.erl
@@ -23,13 +23,12 @@ init(Type, _State) ->
 
 
 %% Lock
-lock(AppInfo, State) ->
-  %%check_type_support(),
-  %%Name = rebar_app_info:name(AppInfo),
-  %%BaseDir = filename:join([rebar_dir:root_dir(State), "_elixir_build/", Name]),  %% Base app.
-  %%rebar_api:console("Dependency ~s already exists~n", [BaseDir]),
-  %%lock_(BaseDir, rebar_app_info:source(AppInfo)).
-  rebar_app_info:source(AppInfo).
+lock(AppInfo, _State) ->
+  check_type_support(),
+  Name = rebar_app_info:name(AppInfo),
+  {ok, CurrDir} = file:get_cwd(),
+  BaseDir = filename:join([CurrDir, "_elixir_build/", Name]),  %% Base app.
+  lock_(BaseDir, rebar_app_info:source(AppInfo)).
 
 lock_(AppDir, {iex_git, Url, _}) ->
   lock_(AppDir, {iex_git, Url});
@@ -52,7 +51,7 @@ lock_(AppDir, {iex_git, Url}) ->
 
 %% Download download
 download(TmpDir, AppInfo, State, _) ->
-  %%check_type_support(),
+  check_type_support(),
   Name = rebar_app_info:name(AppInfo),
   BaseDir = filename:join([rebar_dir:root_dir(State), "_elixir_build/", Name]),  %% Base app.
   case download_(BaseDir, rebar_app_info:source(AppInfo), State) of
@@ -70,7 +69,6 @@ download(TmpDir, AppInfo, State, _) ->
     Error ->
       {error, Error}
   end.
-
 
 download_(Dir, {iex_git, Url}, State) ->
   ?WARN("WARNING: It is recommended to use {branch, Name}, {tag, Tag} or {ref, Ref}, otherwise updating the dep may not work as expected.", []),
@@ -97,33 +95,31 @@ download_(Dir, {iex_git, Url, Rev}, _State) ->
   git_clone(rev, git_vsn(), Url, Dir, Rev).
 
 
-
 %% Needs Update
 needs_update(AppInfo, _) ->
-  %%check_type_support(),
-  %%needs_update_(rebar_app_info:dir(AppInfo), rebar_app_info:source(AppInfo)).
-  false.
+  check_type_support(),
+  Name = rebar_app_info:name(AppInfo),
+  {ok, CurrDir} = file:get_cwd(),
+  BaseDir = filename:join([CurrDir, "_elixir_build/", Name]),  %% Base app.
+  needs_update_(BaseDir, rebar_app_info:source(AppInfo)).
 
-
-needs_update_(Dir, {git, Url, {tag, Tag}}) ->
+needs_update_(Dir, {iex_git, Url, {tag, Tag}}) ->
   {ok, Current} = rebar_utils:sh(?FMT("git describe --tags --exact-match", []), [{cd, Dir}]),
   Current1 = rebar_string:trim(rebar_string:trim(Current, both, "\n"), both, "\r"),
   ?DEBUG("Comparing git tag ~ts with ~ts", [Tag, Current1]),
   not ((Current1 =:= Tag) andalso compare_url(Dir, Url));
 
-needs_update_(Dir, {git, Url, {branch, Branch}}) ->
-  %% Fetch remote so we can check if the branch has changed
+needs_update_(Dir, {iex_git, Url, {branch, Branch}}) ->
   SafeBranch = rebar_utils:escape_chars(Branch),
   {ok, _} = rebar_utils:sh(?FMT("git fetch origin ~ts", [SafeBranch]), [{cd, Dir}]),
-  %% Check for new commits to origin/Branch
   {ok, Current} = rebar_utils:sh(?FMT("git log HEAD..origin/~ts --oneline", [SafeBranch]), [{cd, Dir}]),
   ?DEBUG("Checking git branch ~ts for updates", [Branch]),
   not ((Current =:= []) andalso compare_url(Dir, Url));
 
-needs_update_(Dir, {git, Url, "master"}) ->
+needs_update_(Dir, {iex_git, Url, "master"}) ->
   needs_update_(Dir, {git, Url, {branch, "master"}});
 
-needs_update_(Dir, {git, _, Ref}) ->
+needs_update_(Dir, {iex_git, _, Ref}) ->
   {ok, Current} = rebar_utils:sh(?FMT("git rev-parse --short=7 -q HEAD", []), [{cd, Dir}]),
   Current1 = rebar_string:trim(rebar_string:trim(Current, both, "\n"),  both, "\r"),
   
@@ -151,20 +147,20 @@ make_vsn(_, _) ->
 %%=============================
 %% Private Functions
 %%=============================
-%% check_type_support() ->
-%%   case get({is_supported, ?MODULE}) of
-%%     true ->
-%%       ok;
-%%     _ ->
-%%       case rebar_utils:sh("git --version", [{return_on_error, true},
-%%                                             {use_stdout, false}]) of
-%%         {error, _} ->
-%%           ?ABORT("git not installed", []);
-%%         _ ->
-%%           put({is_supported, ?MODULE}, true),
-%%           ok
-%%       end
-%%   end.
+check_type_support() ->
+  case get({is_supported, ?MODULE}) of
+    true ->
+      ok;
+    _ ->
+      case rebar_utils:sh("git --version", [{return_on_error, true},
+                                            {use_stdout, false}]) of
+        {error, _} ->
+          ?ABORT("git not installed", []);
+        _ ->
+          put({is_supported, ?MODULE}, true),
+          ok
+      end
+  end.
 
 
 compare_url(Dir, Url) ->
@@ -276,85 +272,6 @@ git_vsn_fetch() ->
 
 
 %% Internal functions
-
-collect_default_refcount(Dir) ->
-  %% Get the tag timestamp and minimal ref from the system. The
-  %% timestamp is really important from an ordering perspective.
-  case rebar_utils:sh("git log -n 1 --pretty=format:\"%h\n\" ",
-                      [{use_stdout, false},
-                       return_on_error,
-                       {cd, Dir}]) of
-    {error, _} ->
-      ?WARN("Getting log of git dependency failed in ~ts. Falling back to version 0.0.0", [rebar_dir:get_cwd()]),
-      {plain, "0.0.0"};
-    {ok, String} ->
-      RawRef = rebar_string:trim(String, both, "\n"),
-
-      {Tag, TagVsn} = parse_tags(Dir),
-      {ok, RawCount} =
-        case Tag of
-          undefined ->
-            AbortMsg2 = "Getting rev-list of git depedency failed in " ++ Dir,
-            {ok, PatchLines} = rebar_utils:sh("git rev-list HEAD",
-                                              [{use_stdout, false},
-                                               {cd, Dir},
-                                               {debug_abort_on_error, AbortMsg2}]),
-            rebar_utils:line_count(PatchLines);
-          _ ->
-            get_patch_count(Dir, Tag)
-        end,
-      {TagVsn, RawRef, RawCount}
-  end.
-
-build_vsn_string(Vsn, RawRef, Count) ->
-  %% Cleanup the tag and the Ref information. Basically leading 'v's and
-  %% whitespace needs to go away.
-  RefTag = [".ref", re:replace(RawRef, "\\s", "", [global, unicode])],
-
-  %% Create the valid [semver](http://semver.org) version from the tag
-  case Count of
-    0 ->
-      rebar_utils:to_list(Vsn);
-    _ ->
-      rebar_utils:to_list([Vsn, "+build.", integer_to_list(Count), RefTag])
-  end.
-
-get_patch_count(Dir, RawRef) ->
-  AbortMsg = "Getting rev-list of git dep failed in " ++ Dir,
-  Ref = re:replace(RawRef, "\\s", "", [global, unicode]),
-  Cmd = io_lib:format("git rev-list ~ts..HEAD",
-                      [rebar_utils:escape_chars(Ref)]),
-  {ok, PatchLines} = rebar_utils:sh(Cmd,
-                                    [{use_stdout, false},
-                                     {cd, Dir},
-                                     {debug_abort_on_error, AbortMsg}]),
-  rebar_utils:line_count(PatchLines).
-
-
-parse_tags(Dir) ->
-  %% Don't abort on error, we want the bad return to be turned into 0.0.0
-  case rebar_utils:sh("git -c color.ui=false log --oneline --no-walk --tags --decorate",
-                      [{use_stdout, false}, return_on_error, {cd, Dir}]) of
-    {error, _} ->
-      {undefined, "0.0.0"};
-    {ok, Line} ->
-      case re:run(Line, "(\\(|\\s)(HEAD[^,]*,\\s)tag:\\s(v?([^,\\)]+))", [{capture, [3, 4], list}, unicode]) of
-        {match,[Tag, Vsn]} ->
-          {Tag, Vsn};
-        nomatch ->
-          case rebar_utils:sh("git describe --tags --abbrev=0",
-                              [{use_stdout, false}, return_on_error, {cd, Dir}]) of
-            {error, _} ->
-              {undefined, "0.0.0"};
-            %% strip the v prefix if it exists like is done in the above match
-            {ok, [$v | LatestVsn]} ->
-              {undefined, rebar_string:trim(LatestVsn, both, "\n")};
-            {ok, LatestVsn} ->
-              {undefined, rebar_string:trim(LatestVsn,both, "\n")}
-          end
-      end
-  end.
-
 git_clone_options() ->
   Option = case os:getenv("REBAR_GIT_CLONE_OPTIONS") of 
              false -> "" ;       %% env var not set

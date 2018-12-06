5bb01d9b77b01d884c13fa4bfc3f4d8e54ecdb31
Andres Mareca <andres.mareca@gmail.com>
2018-12-06 11:09:44 +0100

    Fix compile

--- src/rebar3_elixir.erl
+++ src/rebar3_elixir.erl
@@ -3,26 +3,4 @@
 -export([init/1]).
 
 init(State) ->
-  {ok, State0} = rebar3_elixir_compile_prv_ex_compiler:init(State),
-  State1 = rebar_state:add_resource(State0, {iex_git, rebar3_resource_git}),
-  State2 = rebar_state:add_resource(State1, {iex, rebar3_elixir_compile_resource}),
-  {_BinDir, _Env, _Config, LibDir} = rebar3_elixir_compile_util:get_details(State2),
-  code:add_patha(filename:join(LibDir, "elixir/ebin")),
-  code:add_patha(filename:join(LibDir, "mix/ebin")),
-  code:add_patha(filename:join(LibDir, "logger/ebin")),
-  State3 = State2, %%rebar3_elixir_compile_util:add_deps_to_path(State2),
-  Dir = filename:join(rebar_dir:root_dir(State3), "elixir_libs/"),
-  State4 = case rebar_utils:list_dir(Dir) of
-             {ok, Apps} -> rebar3_elixir_compile_util:add_deps_to_path(State3, Apps, false);
-             _ -> State3
-           end,
-  RelxConfig = rebar_state:get(State4, relx, []),
-  NewRelxConfig = case lists:keyfind(lib_dirs, 1, RelxConfig) of
-                    {lib_dirs, OldLibDir} -> 
-                      NewLibDir = OldLibDir ++ [LibDir],
-                      lists:keyreplace(lib_dirs, 1, RelxConfig, {lib_dirs, NewLibDir});
-                    false -> 
-                      [{lib_dirs, [LibDir]}] ++ RelxConfig  
-                  end,
-  State6 = rebar_state:set(State4, relx, NewRelxConfig),
-  {ok, State6}.
+  {ok, State}.

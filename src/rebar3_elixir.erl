fd1a01cc6504d17c52ee915d0162d7b8cbcae34b
Andres Mareca <andres.mareca@gmail.com>
2019-02-01 17:01:02 +0100

    Prepare compiler

--- src/rebar3_elixir.erl
+++ src/rebar3_elixir.erl
@@ -3,9 +3,7 @@
 -export([init/1]).
 
 init(State) ->
-  {ok, State0} = rebar3_elixir_compiler:init(State),
-  State1 = rebar3_elixir_utils:add_elixir(State0),
-  State2 = rebar_state:add_resource(State1, {iex_git, rebar3_elixir_git}),
-  State3 = rebar_state:add_resource(State2, {iex, rebar3_elixir_hex}),
-  State4 = rebar_state:add_resource(State3, {iex_dep, rebar3_elixir_dep}),
-  {ok, State4}.
+  State1 = rebar3_elixir_utils:add_elixir(State),
+  State2 = rebar_state:add_resource(State1, {iex, rebar3_elixir_hex}),
+  State3 = rebar_state:add_project_builder(State2, mix, rebar3_elixir_compiler),
+  {ok, State3}.
--- src/rebar3_elixir_compiler.erl
+++ src/rebar3_elixir_compiler.erl
@@ -1,36 +1,32 @@
 -module(rebar3_elixir_compiler).
 
--export([init/1, do/1, format_error/1]).
-
--define(PROVIDER, rebar3_elixir).
--define(DEPS, [{default, compile}]).
--define(NAMESPACE, iex).
+-export([build/1,
+         format_error/1]).
 
 %% ===================================================================
 %% Public API
 %% ===================================================================
--spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
-    Provider = 
-    providers:create(
-      [
-       {name, ?PROVIDER},            % The 'user friendly' name of the task
-       {module, ?MODULE},            % The module implementation of the task
-       {namespace, ?NAMESPACE},
-       {bare, true},                 % The task can be run by the user, always true
-       {deps, ?DEPS},                % The list of dependencies
-       {example, "rebar3 elixir compiler"}, % How to use the plugin
-       {opts, []},                   % list of options understood by the plugin
-       {short_desc, "A rebar plugin"},
-       {desc, "A rebar plugin"}
-      ]),
-  {ok, rebar_state:add_provider(State, Provider)}.
+build(AppInfo) ->
+  AppDir = rebar_app_info:dir(AppInfo),
+  BuildDir = filename:join(AppDir, "../"),
+  rebar_utils:sh("mix deps.get", [
+                                  {cd, AppDir}, 
+                                  {use_stdout, false}, 
+                                  abort_on_error]),
+  rebar_utils:sh("mix compile", [
+                                 {cd, AppDir}, 
+                                 {use_stdout, false}, 
+                                 abort_on_error,
+                                 {env, [
+                                        {"MIX_ENV", rebar3_elixir_utils:get_env()}
+                                       ]
+                                 }]),
+  ok.
 
-
--spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
-do(State) ->
-  {ok, State}.
-
--spec format_error(any()) ->  iolist().
+format_error({mix_not_found, Name}) ->
+  io_lib:format("Elixir and mix must be installed to build application ~ts. "
+                "Install Elixir or check your path and try again.", [Name]);
+format_error({mix_compile_failed, Name, _Error}) ->
+  io_lib:format("Failed to compile application ~ts with mix", [Name]);
 format_error(Reason) ->
-  io_lib:format("~p", [Reason]).
+  io_lib:format("~p", Reason).

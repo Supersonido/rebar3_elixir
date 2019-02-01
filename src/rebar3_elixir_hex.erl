b16da6b3c0a8085d65821e6225912ae1b6068f98
Andres Mareca <andres.mareca@gmail.com>
2019-02-01 17:01:51 +0100

    Adapt hex.pm module

--- src/rebar3_elixir_hex.erl
+++ src/rebar3_elixir_hex.erl
@@ -24,9 +24,10 @@ lock(AppInfo, _) ->
 
 
 %% Download download
-download(TmpDir, AppInfo, ResorceState, _State) ->
-  fetch_and_compile(ResorceState, TmpDir, rebar_app_info:source(AppInfo)).
-  
+download(Dir, AppInfo, State, _) ->
+  Pkg = rebar_app_info:source(AppInfo),
+  CDN = cdn(State),
+  fetch(Pkg, CDN, Dir).
 
 %% Needs Update
 needs_update(AppInfo, _) ->
@@ -49,31 +50,6 @@ make_vsn(_, _) ->
 %%=================================
 %% Private function
 %%=================================
-fetch_and_compile(State, Dir, {iex, Name, _Vsn} = Pkg) ->
-  CDN = cdn(State),  %% Get CDN
-  BaseDir = filename:join([rebar_dir:root_dir(State), "_elixir_build/", Name]),  %% Base app.
-  fetch(Pkg, CDN, BaseDir), %% Fech from hex
-  case rebar3_elixir_utils:compile_app(State, BaseDir) of  %% Compile elixir app.
-    {ok, Env} ->
-      %% Copy app code into rebar tmp folder
-      Source = filename:join([BaseDir, "_build/", Env, "lib", Name]),
-      ec_file:copy(Source, Dir, [recursive]),
-      %% Copy deps into _build path
-      DepsSource = filename:join([BaseDir, "_build/", Env, "lib"]),
-      {ok, Files} = rebar_utils:list_dir(DepsSource),
-      Deps = Files -- [Name],
-      rebar3_elixir_utils:move_deps(Deps, DepsSource, State),
-      %% Generate rebar.lock for elixir app
-      Lock = rebar3_elixir_utils:create_rebar_lock_from_mix(BaseDir, Deps), 
-      %% Add elixir as depencende
-      Lock2 = rebar3_elixir_utils:add_elixir_to_dependence(State, Lock),
-      %% Save Lock.
-      rebar3_elixir_utils:save_rebar_lock(Dir, Lock2),
-      ok;
-    _ ->
-      {error, <<"Something happen">>}
-  end.
-
 cdn(State) ->
   Opts = rebar_state:get(State, elixir_opts, []),
   CDNSite = proplists:get_value(cdn, Opts, ?DEFAULT_CDN_SITE),

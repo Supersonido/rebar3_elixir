1948c2d945b7677dd6b51884b995bce4d43b22c6
Andres Mareca <andres.mareca@gmail.com>
2019-03-22 16:14:42 +0100

    Small fix

--- src/rebar3_elixir.erl
+++ src/rebar3_elixir.erl
@@ -6,6 +6,14 @@ init(State) ->
   State1 = rebar3_elixir_utils:add_elixir(State),
   State2 = rebar_state:add_resource(State1, {iex, rebar3_elixir_hex}),
   State3 = rebar_state:add_resource(State2, {iex_dep, rebar3_elixir_dep}),
-  State4 = rebar_state:add_project_builder(State3, mix, rebar3_elixir_compiler),
+  
+  
+  State4 =
+    try rebar_state:add_project_builder(State3, mix, rebar3_elixir_compiler) of
+        _ -> ok
+    catch
+      _ -> State3
+    end,
+  
   State5 = rebar3_elixir_utils:add_elixir_to_path(State4),
   {ok, State5}.

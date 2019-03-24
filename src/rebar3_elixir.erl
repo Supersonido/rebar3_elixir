9b70eda934d204c9ef28124b0e4e495ba259a82a
Andres Mareca <andres.mareca@gmail.com>
2019-03-24 22:18:38 +0100

    Fix compilation error in some cases

--- src/rebar3_elixir.erl
+++ src/rebar3_elixir.erl
@@ -6,13 +6,10 @@ init(State) ->
   State1 = rebar3_elixir_utils:add_elixir(State),
   State2 = rebar_state:add_resource(State1, {iex, rebar3_elixir_hex}),
   State3 = rebar_state:add_resource(State2, {iex_dep, rebar3_elixir_dep}),
-  
   State4 =
-    try 
-      rebar_state:add_project_builder(State3, mix, rebar3_elixir_compiler)
-    catch
-      _ -> State3
+    if erlang:function_exported(rebar_state, add_project_builder, 3) -> 
+        rebar_state:add_project_builder(State3, mix, rebar3_elixir_compiler);
+       true -> State3
     end,
-  
   State5 = rebar3_elixir_utils:add_elixir_to_path(State4),
   {ok, State5}.
